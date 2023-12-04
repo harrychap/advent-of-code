91ff22236c2c12bd2b38da322161a0d89ada6e74
harrychap <harrychaplain@hotmail.co.uk>
2023-12-04 19:19:44 +0000

    2022 part 5

--- src/main/kotlin/advent2022/Task05.kt
+++ src/main/kotlin/advent2022/Task05.kt
@@ -1,15 +1,49 @@
 package advent2022
 
 import Task
+import advent2022.Task05.getStack
 import readInput
 
 object Task05 : Task {
-    override fun partA(): Int {
-        println(parseInput())
-        return 0
+
+    private fun getStack() = mapOf(
+        1 to mutableListOf('J', 'H', 'P', 'M', 'S', 'F', 'N', 'V'),
+        2 to mutableListOf('S', 'R', 'L', 'M', 'J', 'D', 'Q'),
+        3 to mutableListOf('N', 'Q', 'D', 'H', 'C', 'S', 'W', 'B'),
+        4 to mutableListOf('R', 'S', 'C', 'L'),
+        5 to mutableListOf('M', 'V', 'T', 'P', 'F', 'B'),
+        6 to mutableListOf('T', 'R', 'Q', 'N', 'C'),
+        7 to mutableListOf('G', 'V', 'R'),
+        8 to mutableListOf('C', 'Z', 'S', 'P', 'D', 'L', 'R'),
+        9 to mutableListOf('D', 'S', 'J', 'V', 'G', 'P', 'B', 'F'),
+    )
+    override fun partA(): List<Char> {
+        val partAStack = getStack()
+        parseInput().forEach { (move, from, to) ->
+            repeat(move) {
+                val last = partAStack[from]?.last()
+                if (last != null){
+                    partAStack[to]?.add(last)
+                    partAStack[from]?.removeLast()
+                }
+            }
+        }
+        return partAStack.map { it.value.last() }
     }
 
-    override fun partB(): Int = 0
+    override fun partB(): List<Char> {
+        val partBStack = getStack()
+        parseInput().forEach { (move, from, to) ->
+            val last = partBStack[from]?.takeLast(move)
+            last?.forEach {
+                partBStack[to]?.add(it)
+                partBStack[from]?.removeLast()
+            }
+        }
+        println(partBStack)
+        return partBStack.map { it.value.last() }
+    }
 
-    private fun parseInput() = readInput("2022-05.txt").split("\n")
+    private fun parseInput() = readInput("2022-05.txt").split("\n\n").last().split('\n')
+        .map { it.split(' ').mapNotNull { it.toIntOrNull() } }
 }
