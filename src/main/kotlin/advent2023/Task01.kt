8ef6f850f6d8db0c0d2112476cebe61e06b54ba4
harrychap <harrychaplain@hotmail.co.uk>
2023-12-06 13:58:40 +0000

    Update Task01.kt

--- src/main/kotlin/advent2023/Task01.kt
+++ src/main/kotlin/advent2023/Task01.kt
@@ -19,18 +19,19 @@ object Task01 : Task {
     )
 
     override fun partA(): Int = parseInput().sumOf { line ->
-        "${ line.find { it.isDigit() }}${ line.findLast { it.isDigit() }}".toInt()
+        "${line.find { it.isDigit() }}${line.findLast { it.isDigit() }}".toInt()
     }
 
     override fun partB(): Int = parseInput().sumOf { line ->
-        val firstWordNumber =  numberMap.keys.map {
+        val firstWordNumber = numberMap.keys.map {
             line.windowed(5, 1)
-                .firstNotNullOfOrNull { window -> numberMap.keys.firstOrNull() { key -> window.contains(key) } }
-            }.firstOrNull() ?: "nothing"
+                .firstNotNullOfOrNull { window -> numberMap.keys.firstOrNull { key -> window.contains(key) } }
+        }.firstOrNull() ?: "nothing"
 
-        val lastWordNumber =  numberMap.keys.map {
+        val lastWordNumber = numberMap.keys.map {
             line.windowed(5, 1)
-                .mapNotNull { window -> numberMap.keys.lastOrNull() { key -> window.contains(key) } }.lastOrNull() }
+                .mapNotNull { window -> numberMap.keys.lastOrNull { key -> window.contains(key) } }.lastOrNull()
+        }
             .lastOrNull() ?: "nothing"
 
         val firstWord = numberMap[firstWordNumber]
@@ -38,8 +39,10 @@ object Task01 : Task {
         val firstNumber = line.find { it.isDigit() }?.digitToIntOrNull()
         val lastNumber = line.findLast { it.isDigit() }?.digitToIntOrNull()
 
-        val firstCoord = if(line.substringBefore(firstWordNumber).contains(firstNumber.toString())) firstNumber else firstWord
-        val lastCoord = if(line.substringAfterLast(lastWordNumber).contains(lastNumber.toString())) lastNumber else lastWord
+        val firstCoord =
+            if (line.substringBefore(firstWordNumber).contains(firstNumber.toString())) firstNumber else firstWord
+        val lastCoord =
+            if (line.substringAfterLast(lastWordNumber).contains(lastNumber.toString())) lastNumber else lastWord
 
         "$firstCoord$lastCoord".toInt()
     }
