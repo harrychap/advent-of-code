ccecbc9b5732d62ab67bbe9a4157a7d91b94ec2e
harrychap <harrychaplain@hotmail.co.uk>
2023-12-09 15:51:44 +0000

    Task 4 finished

--- src/main/kotlin/advent2023/Task04.kt
+++ src/main/kotlin/advent2023/Task04.kt
@@ -2,6 +2,8 @@ package advent2023
 
 import Task
 import utils.readInput
+import java.util.*
+import kotlin.collections.HashMap
 import kotlin.math.pow
 
 object Task04 : Task {
@@ -11,8 +13,29 @@ object Task04 : Task {
         (1 shl count).coerceAtLeast(0)
     }
 
+    override fun partB(): Int {
+        val cardsAndScores = parseInput().associate { (game, results) ->
+            game to results.last().count { results.first().contains(it) }
+        }
 
-    override fun partB(): Int = 0 // TODO
+        val cardsToProcess = PriorityQueue<Int>()
+        cardsToProcess.addAll(cardsAndScores.keys)
+
+        var cardCount = 0
+
+        while (cardsToProcess.isNotEmpty()) {
+            ++cardCount
+            val cardNumber = cardsToProcess.poll()
+            val cardNoOfWins = cardsAndScores[cardNumber] ?: 0
+
+            repeat(cardNoOfWins) { cardNo ->
+                val nextCard = cardNumber + (cardNo + 1)
+                cardsAndScores[nextCard].let { cardsToProcess.add(nextCard) }
+            }
+        }
+
+        return cardCount
+    }
 
     private fun parseInput() =
         readInput("2023-04.txt").split("\n")
