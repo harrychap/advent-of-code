c4ce00a202e2c99d033f2567326c66c6dfd6a64c
harrychap <harrychaplain@hotmail.co.uk>
2023-12-13 17:42:38 +0000

    Update Task07.kt

--- src/main/kotlin/advent2023/Task07.kt
+++ src/main/kotlin/advent2023/Task07.kt
@@ -13,7 +13,11 @@ object Task07 : Task {
     private val cardOrder = listOf("A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2")
     private val cardOrderWithJoker = listOf("A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J")
 
-    override fun partA(): Int {
+    override fun partA(): Int = calculateTotal(cardOrder)
+
+    override fun partB(): Int = calculateTotal(cardOrderWithJoker)
+
+    private fun calculateTotal(cardOrder: List<String>): Int {
         val fiveOfAKinds = mutableListOf<Pair<String, Int>>()
         val fourOfAKinds = mutableListOf<Pair<String, Int>>()
         val threeOfAKinds = mutableListOf<Pair<String, Int>>()
@@ -23,14 +27,27 @@ object Task07 : Task {
         val distinct = mutableListOf<Pair<String, Int>>()
 
         parseInput().forEach { (hand, bid) ->
-            when {
-                hand.isFiveOfAKind() -> fiveOfAKinds.add(hand to bid)
-                hand.isFourOfAKind() -> fourOfAKinds.add(hand to bid)
-                hand.isThreeOfAKind() -> threeOfAKinds.add(hand to bid)
-                hand.isFullHouse() -> fullHouses.add(hand to bid)
-                hand.isTwoPair() -> twoPairs.add(hand to bid)
-                hand.isPair() -> pairs.add(hand to bid)
-                else -> distinct.add(hand to bid)
+            if (cardOrder.last() == "J") {
+                val handsWithJoker = cardOrder.map { hand.replace("J", it) }
+                when {
+                    handsWithJoker.any { it.isFiveOfAKind() } -> fiveOfAKinds.add(hand to bid)
+                    handsWithJoker.any { it.isFourOfAKind() } -> fourOfAKinds.add(hand to bid)
+                    handsWithJoker.any { it.isThreeOfAKind() } -> threeOfAKinds.add(hand to bid)
+                    handsWithJoker.any { it.isFullHouse() } -> fullHouses.add(hand to bid)
+                    handsWithJoker.any { it.isTwoPair() } -> twoPairs.add(hand to bid)
+                    handsWithJoker.any { it.isPair() } -> pairs.add(hand to bid)
+                    else -> distinct.add(hand to bid)
+                }
+            } else {
+                when {
+                    hand.isFiveOfAKind() -> fiveOfAKinds.add(hand to bid)
+                    hand.isFourOfAKind() -> fourOfAKinds.add(hand to bid)
+                    hand.isThreeOfAKind() -> threeOfAKinds.add(hand to bid)
+                    hand.isFullHouse() -> fullHouses.add(hand to bid)
+                    hand.isTwoPair() -> twoPairs.add(hand to bid)
+                    hand.isPair() -> pairs.add(hand to bid)
+                    else -> distinct.add(hand to bid)
+                }
             }
         }
 
@@ -43,41 +60,6 @@ object Task07 : Task {
         fullHouses.sortCards(cardOrder).forEach { allCardsOrdered.add(it) }
         fourOfAKinds.sortCards(cardOrder).forEach { allCardsOrdered.add(it) }
         fiveOfAKinds.sortCards(cardOrder).forEach { allCardsOrdered.add(it) }
-
-        return allCardsOrdered.sumOf { (key, value) -> (allCardsOrdered.indexOf(key to value) + 1) * value }
-    }
-
-    override fun partB(): Int {
-        val fiveOfAKinds = mutableListOf<Pair<String, Int>>()
-        val fourOfAKinds = mutableListOf<Pair<String, Int>>()
-        val threeOfAKinds = mutableListOf<Pair<String, Int>>()
-        val fullHouses = mutableListOf<Pair<String, Int>>()
-        val twoPairs = mutableListOf<Pair<String, Int>>()
-        val pairs = mutableListOf<Pair<String, Int>>()
-        val distinct = mutableListOf<Pair<String, Int>>()
-
-        parseInput().forEach { (hand, bid) ->
-            val handsWithJoker = cardOrderWithJoker.map { hand.replace("J", it) }
-            when {
-                handsWithJoker.any { it.isFiveOfAKind() } -> fiveOfAKinds.add(hand to bid)
-                handsWithJoker.any { it.isFourOfAKind() } -> fourOfAKinds.add(hand to bid)
-                handsWithJoker.any { it.isThreeOfAKind() } -> threeOfAKinds.add(hand to bid)
-                handsWithJoker.any { it.isFullHouse() } -> fullHouses.add(hand to bid)
-                handsWithJoker.any { it.isTwoPair() } -> twoPairs.add(hand to bid)
-                handsWithJoker.any { it.isPair() } -> pairs.add(hand to bid)
-                else -> distinct.add(hand to bid)
-            }
-        }
-
-        val allCardsOrdered = mutableListOf<Pair<String, Int>>()
-
-        distinct.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        pairs.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        twoPairs.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        threeOfAKinds.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        fullHouses.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        fourOfAKinds.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
-        fiveOfAKinds.sortCards(cardOrderWithJoker).forEach { allCardsOrdered.add(it) }
 
         return allCardsOrdered.sumOf { (key, value) -> (allCardsOrdered.indexOf(key to value) + 1) * value }
     }
